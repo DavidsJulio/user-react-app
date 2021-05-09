309d31647fb8be474cd98cd342c96c150bda8aa1
DavidsJulio <davidsjulio97@gmail.com>
2021-05-09 13:16:55 +0100

    Button component and styling; Also changed the default button for the component in AddUser.

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 
@@ -17,7 +18,7 @@ function AddUser(props) {
         <label htmlFor="age">Age (years)</label>
         <input id="age" type="number" />
         {/* button */}
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
